4f055a031dcb1b2bf707182df3cc5f32631b0c77
zhejiang <zhejiang.uk@gmail.com>
2022-01-26 17:25:32 +0000

    Adding raw module and TB for GHT.

---
+++ src/main/scala/guardiancouncil/GHT.scala
@@ -0,0 +1,45 @@
+package guardiancouncil
+
+
+import chisel3._
+import chisel3.util._
+import chisel3.stage.ChiselStage
+
+
+// *Driver is used for verilog generation
+object GHT_Driver extends App {
+  (new ChiselStage).emitVerilog(new GHT(40, 64),  args)
+}
+
+
+class GHT (Widith_CorePC: Int, Width_GHTPCCount: Int) extends Module {
+    val io = IO(new Bundle {
+    val core_reset_vector_in = Input(UInt(Widith_CorePC.W))
+    val core_pc_in = Input(UInt(Widith_CorePC.W))
+    val ght_pc_count_out = Output(UInt(Width_GHTPCCount.W))
+  })
+
+  val core_pc_current_reg = RegInit (0.U(Widith_CorePC.W))
+  val ght_pc_counter_reg = RegInit (0.U(Width_GHTPCCount.W))
+
+  def val_incr1 (currnt: UInt): UInt = {
+      var nxt = currnt + 1.U;
+      nxt
+  }
+
+  // Core: PC shaddow register
+  when (core_pc_current_reg =/= io.core_pc_in) {
+    core_pc_current_reg := io.core_pc_in
+  } .otherwise {
+    core_pc_current_reg := core_pc_current_reg
+  }
+
+  // GHT: PC counter
+  when (core_pc_current_reg =/= io.core_pc_in) {
+    ght_pc_counter_reg := this.val_incr1(ght_pc_counter_reg)
+  } .otherwise {
+    ght_pc_counter_reg := ght_pc_counter_reg
+  }
+
+  io.ght_pc_count_out := ght_pc_counter_reg
+}
---
+++ src/test/scala/guardiancouncil/TB_GHT.scala
@@ -0,0 +1,36 @@
+package guardiancouncil
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+
+class TB_GHT extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "GHT"
+  // test class body here
+  it should "Test guardiancouncil.GHT" in {
+  // test case body here
+    test(new GHT(40, 64)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+    // test body here
+    dut.io.core_reset_vector_in.poke(0x10040.U)
+    dut.io.ght_pc_count_out.expect(0.U)
+
+    //============ Commit 1 instruction ============
+    dut.clock.step(10) // clk+10
+    dut.io.core_pc_in.poke(0x10044.U) // PC+1
+
+    dut.clock.step(1) // clk+1
+    dut.io.ght_pc_count_out.expect(1.U)
+
+    //============ Commit 2 instruction ============
+    dut.clock.step(2) // clk+10
+    dut.io.core_pc_in.poke(0x10048.U) // PC+1
+
+    dut.clock.step(1) // clk+10
+    dut.io.core_pc_in.poke(0x10800.U) // PC JUmp
+    dut.clock.step(1) // clk+1
+    dut.io.ght_pc_count_out.expect(3.U)
+    }
+  }
+  it should "All tested!"
+}
